98aa62a1d70d4f786a0debeb36ab71cfcf11bf89
Moushumi Das <dasm3@myumanitoba.ca>
2021-08-03 12:39:23 -0500

    blog application

---
+++ index.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv')
+const http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const server = http.createServer(app);
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+const mongoose = require('mongoose');
+const authRouter=require('./routes/auth')
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
+});
+
+
+app.use('/api/auth',authRouter)
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+
+})
